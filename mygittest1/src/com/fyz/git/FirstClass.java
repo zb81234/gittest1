0c229acdc1299a4cf72b932a7daa9fd3200d587a
Yikoo <1550940806@qq.com>
2020-07-24 13:06:46 +0800

    提交本地工程gittest1

---
+++ mygittest1/src/com/fyz/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.fyz.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project.");
+        System.out.println("hello git project.");
+        System.out.println("hello git project.");
+    }
+
+    public void test1(){
+        System.out.println("hello git test1.");
+    }
+}
